9435ddbe1accec07ec8cea1491fe1ddef53234ab
Ilja Radusch <ilja.radusch@fokus.fraunhofer.de>
2026-07-09 16:25:34 +0200

    refactor: extract constants and binary helpers into package modules
    Move BinaryTools, is_vin, convert_*, hex_of_value, display_bytes_hex,
    print_value_tabular and the shared constants into zero_log_parser.constants
    and zero_log_parser.binary. The monolith now imports them.

---
+++ src/zero_log_parser/constants.py
@@ -0,0 +1,9 @@
+"""Shared constants for the Zero log parser."""
+
+# Localized time format - use system locale preference
+ZERO_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'  # ISO format is more universal
+# The output from the MBB (via serial port) lists time as GMT-7
+MBB_TIMESTAMP_GMT_OFFSET = -7 * 60 * 60
+
+EMPTY_CSV_VALUE = ''
+CSV_DELIMITER = ';'
